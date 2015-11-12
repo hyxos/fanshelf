73540fea8b485e9d11d2c0cd0c26ee805a58481a
josephskan <josephskan@live.com>
2015-11-12 16:49:50 -0500

    front page in progress

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  $('#login_bttn').on('click',loginWindowClick)
+  function loginWindowClick(){
+    $('#login_window').show();
+  }
+
+  $('#hide_window').on('click',hideWindow)
+  function hideWindow(){
+    $('#login_window').hide();
+  }
+
 });
