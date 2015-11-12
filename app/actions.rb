b69803ac29e49773f9bf90bb8fb66e4ba414e70b
hyxos <dw@1hx.ca>
2015-11-12 21:43:16 +0000

    cleaned up the routes

--- app/actions.rb
+++ app/actions.rb
@@ -35,7 +35,7 @@ post '/user/new' do
       )
     @shelf.user = @user
     @shelf.save
-    redirect "/user/#{@user.id}"
+    redirect "/user"
   else
     erb :'index'
   end
@@ -47,21 +47,20 @@ post '/shelf/:id/delete' do
     @shelf = Shelf.find params[:id]
     @shelf.destroy unless @shelf.name == "Top Shelf"
 
-    redirect "/user/#{@user.id}"
+    redirect "/user"
   end
 end
-# get '/shelf/:id' do
-#   if current_user != []
-#     @user = current_user
-#   end
 
-#   @shelf = Shelf.find params[:id]
+get '/shelf/:id' do
+  @shelf = Shelf.find params[:id]
   
-#   erb :'user/index'
-# end
+  erb :'show_shelf'
+end
 
-get '/user/:id' do
-  @user = User.find params[:id]
+get '/user' do
+  if current_user
+    @user = current_user
+  end
   @shelves = Shelf.where(user_id: @user.id)
   erb :'user/index'
 end
@@ -69,7 +68,7 @@ end
 post '/user/login' do 
   if @user = User.find_by_name(params[:name]).try(:authenticate, params[:password])
     session[:id] = @user.id
-    redirect "/user/#{@user.id}"
+    redirect "/user"
   else 
     @error = "Wrong user name/password"
     redirect '/'
@@ -90,7 +89,7 @@ post '/shelf/new' do
     @shelf.user = @user
   end
   @shelf.save
-  redirect "/user/#{@user.id}"
+  redirect "/user"
 end
 
 post '/shelf/:id/fanfic/new' do
@@ -105,17 +104,17 @@ post '/shelf/:id/fanfic/new' do
   @shelf = Shelf.find params[:id]
   @fanfic.shelf = @shelf
   @fanfic.save
-  redirect "/user/#{@user.id}"
+  redirect "/user"
 end
 
-post '/shelf/:id/fanfic/:id/delete' do
+post '/fanfic/:id/delete' do
   if current_user
     @user = current_user
   end
   @fanfic = Fanfic.find params[:id]
   @fanfic.destroy
 
-  redirect "/user/#{@user.id}"
+  redirect "/user"
 end
 
 get '/submit' do
